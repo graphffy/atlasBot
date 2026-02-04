180394291c6a2a9550a7f78651404f1a5458e2b2
graphffy <graphffy@gmail.com>
2026-02-04 23:11:31 +0300

    add frame for bot

--- bot/bot.go
+++ bot/bot.go
@@ -1,9 +1,41 @@
 package bot
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/mymmrac/telego"
+	tu "github.com/mymmrac/telego/telegoutil"
 )
 
-func main() {
-	fmt.Println("")
+func botStart() {
+	ctx := context.Background()
+	botToken := "8542753002:AAH8H_hIBac4u4ZOKFBLcX_DvMSGbVTn4d8"
+
+	bot, err := telego.NewBot(botToken, telego.WithDefaultDebugLogger())
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	updates, _ := bot.UpdatesViaLongPolling(ctx, nil)
+
+	for {
+		select {
+		case upd := <-updates:
+			if upd.Message != nil {
+				chatId := tu.ID(upd.Message.Chat.ID)
+
+				_, _ = bot.CopyMessage(ctx,
+					tu.CopyMessage(
+						chatId,
+						chatId,
+						upd.Message.MessageID,
+					),
+				)
+			}
+		case <-ctx.Done():
+			break
+		}
+	}
+
 }
