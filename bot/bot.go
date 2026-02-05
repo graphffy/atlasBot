fff4cf6937e0d164b26eff2cf44c48f1fb0c6e12
graphffy <graphffy@gmail.com>
2026-02-05 23:47:03 +0300

    add fsm functional

--- bot/bot.go
+++ bot/bot.go
@@ -3,14 +3,19 @@ package bot
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/mymmrac/telego"
 	tu "github.com/mymmrac/telego/telegoutil"
 )
 
+var waitInput = make(map[int64]bool)
+var userInput = make(map[int64]string)
+
 func botStart() {
+
 	ctx := context.Background()
-	botToken := "8542753002:AAH8H_hIBac4u4ZOKFBLcX_DvMSGbVTn4d8"
+	botToken := os.Getenv("TOKEN")
 
 	bot, err := telego.NewBot(botToken, telego.WithDefaultDebugLogger())
 	if err != nil {
@@ -20,16 +25,46 @@ func botStart() {
 	updates, _ := bot.UpdatesViaLongPolling(ctx, nil)
 
 	for upd := range updates {
+		chatId := tu.ID(upd.Message.Chat.ID)
 		if upd.Message != nil {
-			chatId := tu.ID(upd.Message.Chat.ID)
+			if upd.Message.Text == "/start" {
+				bot.SendMessage(ctx,
+					tu.Message(chatId,
+						"Hello!"),
+				)
+			}
 
-			_, _ = bot.CopyMessage(ctx,
-				tu.CopyMessage(
-					chatId,
-					chatId,
-					upd.Message.MessageID,
-				),
-			)
+			if upd.Message.Text == "/input" {
+
+				message := tu.Message(chatId,
+					"Отправьте данные в формате ГГГГ-ММ-ДД/МинВремяотправления/МаксВремяотправления/городОтправки/городПрибытия/времяПоиска/времяЗапроса")
+
+				bot.SendMessage(ctx, message)
+
+				waitInput[chatId.ID] = true
+
+				continue
+
+			}
+
+			if upd.Message.Text == "/myInput" {
+
+				message := tu.Message(chatId,
+					userInput[chatId.ID])
+
+				bot.SendMessage(ctx, message)
+
+				continue
+
+			}
+
+			if waitInput[chatId.ID] == true {
+
+				userInput[chatId.ID] = upd.Message.Text
+
+				continue
+
+			}
 		}
 
 	}
